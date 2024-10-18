ac5f7bd814019e710bc09b4bc76e91baccdd3dd3
zero-plusplus <git@zero-plusplus.com>
2024-10-18 17:50:52 +0900

    Replace regular expressions in comment.ts with combinators

--- packages/autohotkey-tmlanguage/src/autohotkeyl/repository/comment.ts
+++ packages/autohotkey-tmlanguage/src/autohotkeyl/repository/comment.ts
@@ -1,21 +1,24 @@
 import { Repository, RuleName } from '../../constants';
+import { anyChars0, capture, chr, endAnchor, inlineSpace, inlineSpaces0, lookbehind, seq, startAnchor } from '../../oniguruma';
 import type { MatchRule, PatternsRule, Repositories, ScopeName } from '../../types';
-import { createUtilities, includeRule } from '../../utils';
+import { createUtilities, includeRule, patternsRule } from '../../utils';
 
 export function createRepositories(scopeName: ScopeName): Repositories {
   const { nameRule } = createUtilities(scopeName);
   return {
     [Repository.Comment]: ((): PatternsRule => {
-      return {
-        patterns: [
-          includeRule(Repository.SingleLineComment),
-          includeRule(Repository.InLineComment),
-        ],
-      };
+      return patternsRule(
+        includeRule(Repository.SingleLineComment),
+        includeRule(Repository.InLineComment),
+      );
     })(),
     [Repository.SingleLineComment]: ((): MatchRule => {
       return {
-        match: '(?<=^\\s*)(;.*)$',
+        match: seq(
+          lookbehind(seq(startAnchor(), inlineSpaces0())),
+          capture(seq(chr(';'), anyChars0())),
+          endAnchor(),
+        ),
         captures: {
           1: nameRule(RuleName.SingleLineComment),
         },
@@ -23,7 +26,11 @@ export function createRepositories(scopeName: ScopeName): Repositories {
     })(),
     [Repository.InLineComment]: ((): MatchRule => {
       return {
-        match: '(?<=\\s)(;.*)$',
+        match: seq(
+          lookbehind(inlineSpace()),
+          capture(seq(chr(';'), anyChars0())),
+          endAnchor(),
+        ),
         captures: {
           1: nameRule(RuleName.InLineComment),
         },
