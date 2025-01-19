655fc66b27248d1dd204dbdd56fe4891dfe16c5d
zero-plusplus <git@zero-plusplus.com>
2025-01-19 21:46:03 +0900

    Add fat-arrow syntax tests for v2

--- packages/autohotkey-tmlanguage/test/autohotkey2/expected/expression/call.ts
+++ packages/autohotkey-tmlanguage/test/autohotkey2/expected/expression/call.ts
@@ -1,7 +1,44 @@
+import { dedent } from '@zero-plusplus/utilities/src';
+import { RuleName } from '../../../../src/constants';
 import type { ScopeName } from '../../../../src/types';
+import { name } from '../../../../src/utils';
 import * as common from '../../../common/expression/call';
 import type { ExpectedTestData } from '../../../types';
 
 export function createCallExpressionExpectedData(scopeName: ScopeName): ExpectedTestData[] {
-  return [ ...common.createCallExpressionExpectedData(scopeName) ];
+  return [
+    ...common.createCallExpressionExpectedData(scopeName),
+
+    [
+      dedent`
+        (() => 123)()
+        ((&a, b := 10) => 123)()
+      `,
+      [
+        { text: '(', scopes: name(scopeName, RuleName.OpenParen) },
+        { text: '(', scopes: name(scopeName, RuleName.OpenParen) },
+        { text: ')', scopes: name(scopeName, RuleName.CloseParen) },
+        { text: '=>', scopes: name(scopeName, RuleName.Operator) },
+        { text: '123', scopes: name(scopeName, RuleName.Integer) },
+        { text: ')', scopes: name(scopeName, RuleName.CloseParen) },
+        { text: '(', scopes: name(scopeName, RuleName.OpenParen) },
+        { text: ')', scopes: name(scopeName, RuleName.CloseParen) },
+
+        { text: '(', scopes: name(scopeName, RuleName.OpenParen) },
+        { text: '(', scopes: name(scopeName, RuleName.OpenParen) },
+        { text: '&', scopes: name(scopeName, RuleName.Operator) },
+        { text: 'a', scopes: name(scopeName, RuleName.Variable) },
+        { text: ',', scopes: name(scopeName, RuleName.Comma) },
+        { text: 'b', scopes: name(scopeName, RuleName.Variable) },
+        { text: ':=', scopes: name(scopeName, RuleName.Operator) },
+        { text: '10', scopes: name(scopeName, RuleName.Integer) },
+        { text: ')', scopes: name(scopeName, RuleName.CloseParen) },
+        { text: '=>', scopes: name(scopeName, RuleName.Operator) },
+        { text: '123', scopes: name(scopeName, RuleName.Integer) },
+        { text: ')', scopes: name(scopeName, RuleName.CloseParen) },
+        { text: '(', scopes: name(scopeName, RuleName.OpenParen) },
+        { text: ')', scopes: name(scopeName, RuleName.CloseParen) },
+      ],
+    ],
+  ];
 }
