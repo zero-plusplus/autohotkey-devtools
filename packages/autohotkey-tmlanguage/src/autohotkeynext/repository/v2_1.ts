ce3ef1db29d85fcbb11a509ad2e19f6246c32873
zero-plusplus <git@zero-plusplus.com>
2025-01-21 20:21:31 +0900

    Remove unnecessary file

--- packages/autohotkey-tmlanguage/src/autohotkeynext/repository/v2_1.ts
+++
@@ -1,7 +0,0 @@
-import { Repositories, ScopeName } from '../../types';
-
-export function createRepositories(scopeName: ScopeName): Repositories {
-  // const utils = createUtilities(scopeName);
-
-  return {};
-}
