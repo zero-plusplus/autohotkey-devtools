a07484495765a0dbef2d0f9108436ef6c562eac6
zero-plusplus <git@zero-plusplus.com>
2025-03-23 16:02:31 +0900

    Refactoring `autohotkey` language ID

--- packages/autohotkey-tmlanguage/src/autohotkey/index.ts
+++ packages/autohotkey-tmlanguage/src/autohotkey/index.ts
@@ -1,147 +1,136 @@
-import { alt, anyChars0, capture, char, charRange, endAnchor, group, ignoreCase, inlineSpace, inlineSpaces0, inlineSpaces1, keyword, lookahead, negativeLookahead, ordalt, seq, startAnchor } from '../oniguruma';
-import type { ScopeName, TmLanguage } from '../types';
+import * as constants_v2 from '../autohotkey2/constants';
+import * as constants_v1 from '../autohotkeyl/constants';
+import { alt, anyChars0, capture, char, charRange, endAnchor, escapeOnigurumaTexts, group, ignoreCase, inlineSpaces0, inlineSpaces1, lookahead, lookbehind, negativeLookahead, optional, optseq, ordalt, reluctant, seq, startAnchor } from '../oniguruma';
+import type { Rule, ScopeName, TmLanguage } from '../types';
 import { includeScope, patternsRule } from '../utils';
 
 export function createTmLanguage(): TmLanguage {
   const scopeName: ScopeName = 'autohotkey';
-  const endPattern: string = lookahead(seq(
-    startAnchor(),
-    inlineSpaces0(),
-    ignoreCase(ordalt('#Requires', '#Module')),
-    group(alt(
-      inlineSpace(),
-      char(','),
-      endAnchor(),
-    )),
-  ));
+  const startPattern = group(alt(startAnchor(), '\\G'));
 
   return {
     scopeName: `source.${scopeName}`,
     patterns: [
       { include: '#skip_empty_lines' },
-      { include: '#autohotkeyl_explicit' },
-      { include: '#autohotkeynext_explicit' },
-      { include: '#autohotkey2_explicit' },
-      { include: '#autohotkey2_implicit' },
+      { include: '#autohotkeynext' },
+      { include: '#autohotkey2' },
+      { include: '#autohotkeyl' },
+      { include: '#default' },
     ],
     repository: {
       skip_empty_lines: {
-        begin: seq(startAnchor(), inlineSpaces0(), endAnchor()),
-        while: seq(startAnchor(), inlineSpaces0(), endAnchor()),
+        begin: seq(startPattern, inlineSpaces0(), endAnchor()),
+        while: seq(startPattern, inlineSpaces0(), endAnchor()),
       },
-      autohotkeynext_explicit: {
-        begin: capture(alt(
-          group(seq(
-            startAnchor(),
-            inlineSpaces0(),
-            ignoreCase('#Module'),
-            lookahead(alt(
-              inlineSpace(),
-              char(','),
-              endAnchor(),
-            )),
-            anyChars0(),
-            endAnchor(),
-          )),
-          group(seq(
-            startAnchor(),
-            inlineSpaces0(),
-            ignoreCase('#Requires'),
-            lookahead(alt(
-              inlineSpace(),
-              char(','),
-              endAnchor(),
-            )),
-            inlineSpaces1(),
-            ignoreCase('AutoHotkey'),
-            inlineSpaces1(),
-            ignoreCase('v2'),
-            char('.'),
-            charRange('1', '9'),
-            anyChars0(),
-            endAnchor(),
-          )),
-        )),
-        beginCaptures: {
-          1: patternsRule(includeScope('autohotkeynext')),
-        },
-        end: endPattern,
-        patterns: [ includeScope('autohotkeynext') ],
-      },
-      autohotkey2_explicit: {
-        begin: capture(seq(
-          startAnchor(),
-          inlineSpaces0(),
-          ignoreCase('#Requires'),
-          lookahead(alt(
-            inlineSpace(),
-            char(','),
-            endAnchor(),
-          )),
-          inlineSpaces1(),
-          ignoreCase('AutoHotkey'),
-          inlineSpaces1(),
+      autohotkeynext: createAutoHotkeyRule('autohotkeynext', {
+        startPattern,
+        expressionOperators: constants_v2.expressionOperators,
+        versionPattern: seq(
           ignoreCase('v2'),
           char('.'),
-          char('0'),
-          anyChars0(),
-          endAnchor(),
-        )),
-        beginCaptures: {
-          1: patternsRule(includeScope('autohotkey2')),
-        },
-        end: endPattern,
-        patterns: [ includeScope('autohotkey2') ],
-      },
-      autohotkey2_implicit: {
-        begin: alt(
-          capture(seq(
-            startAnchor(),
-            inlineSpaces0(),
-            ignoreCase('#Requires'),
-            lookahead(alt(
-              inlineSpace(),
-              char(','),
-              endAnchor(),
-            )),
-            anyChars0(),
-            endAnchor(),
-          )),
-          lookahead(seq(
-            startAnchor(),
-            inlineSpaces0(),
-            negativeLookahead(keyword('#Requires')),
-          )),
+          charRange('1', '9'),
         ),
-        beginCaptures: {
-          1: patternsRule(includeScope('autohotkey2')),
-        },
-        end: endPattern,
-        patterns: [ includeScope('autohotkey2') ],
-      },
-      autohotkeyl_explicit: {
-        begin: capture(seq(
-          startAnchor(),
-          inlineSpaces0(),
-          ignoreCase('#Requires'),
-          lookahead(alt(
-            inlineSpace(),
-            char(','),
-            endAnchor(),
-          )),
-          inlineSpaces1(),
-          ignoreCase('AutoHotkey'),
-          inlineSpaces1(),
-          ignoreCase('v1'),
-          anyChars0(),
-          endAnchor(),
-        )),
-        beginCaptures: {
-          1: patternsRule(includeScope('autohotkeyl')),
-        },
-        end: endPattern,
-        patterns: [ includeScope('autohotkeyl') ],
-      },
+      }),
+      autohotkey2: createAutoHotkeyRule('autohotkey2', {
+        startPattern,
+        expressionOperators: constants_v2.expressionOperators,
+        versionPattern: seq(
+          ignoreCase('v2'),
+          optseq(
+            char('.'),
+            optional(char('0')),
+          ),
+        ),
+      }),
+      autohotkeyl: createAutoHotkeyRule('autohotkeyl', {
+        startPattern,
+        expressionOperators: constants_v1.expressionOperators,
+        versionPattern: seq(ignoreCase('v1')),
+      }),
+      default: createDefaultAutoHotkeyRule('autohotkey2', {
+        startPattern,
+        expressionOperators: constants_v2.expressionOperators,
+      }),
     },
   };
 }
+
+// #region helpers
+interface Placeholder_AutoHotkeyRule {
+  startPattern: string;
+  expressionOperators: readonly string[];
+  versionPattern: string;
+}
+function createAutoHotkeyRule(scopeName: ScopeName, placeholder: Placeholder_AutoHotkeyRule): Rule {
+  return {
+    begin: capture(seq(
+      placeholder.startPattern,
+      inlineSpaces0(),
+      ignoreCase('#Requires'),
+      inlineSpaces1(),
+      optseq(
+        ignoreCase('AutoHotkey'),
+        inlineSpaces1(),
+        placeholder.versionPattern,
+        reluctant(anyChars0()),
+        optseq(
+          inlineSpaces1(),
+          capture(seq(
+            char(';'),
+            anyChars0(),
+          )),
+        ),
+      ),
+      endAnchor(),
+    )),
+    beginCaptures: {
+      1: patternsRule(includeScope(scopeName)),
+    },
+    end: lookahead(seq(
+      lookbehind(placeholder.startPattern),
+      inlineSpaces0(),
+      ignoreCase('#Requires'),
+      inlineSpaces1(),
+      negativeLookahead(ordalt(...escapeOnigurumaTexts(placeholder.expressionOperators))),
+    )),
+    patterns: [ includeScope(scopeName) ],
+  };
+}
+interface Placeholder_DefaultAutoHotkeyRule {
+  startPattern: string;
+  expressionOperators: readonly string[];
+}
+function createDefaultAutoHotkeyRule(scopeName: ScopeName, placeholder: Placeholder_DefaultAutoHotkeyRule): Rule {
+  return {
+    begin: alt(
+      capture(seq(
+        placeholder.startPattern,
+        inlineSpaces0(),
+        ignoreCase('#Requires'),
+        inlineSpaces1(),
+        negativeLookahead(ordalt(...escapeOnigurumaTexts(placeholder.expressionOperators))),
+        anyChars0(),
+        endAnchor(),
+      )),
+      negativeLookahead(seq(
+        placeholder.startPattern,
+        inlineSpaces0(),
+        ignoreCase('#Requires'),
+        inlineSpaces1(),
+        ignoreCase('AutoHotkey'),
+      )),
+    ),
+    beginCaptures: {
+      1: patternsRule(includeScope(scopeName)),
+    },
+    end: lookahead(seq(
+      placeholder.startPattern,
+      inlineSpaces0(),
+      ignoreCase('#Requires'),
+      inlineSpaces1(),
+      ignoreCase('AutoHotkey'),
+    )),
+    patterns: [ includeScope(scopeName) ],
+  };
+}
+// #endregion helpers
