6eb144a15c9e6f4c16f1774a4e1d6648110c9801
zero-plusplus <git@zero-plusplus.com>
2025-05-10 00:55:43 +0900

    Add tests

--- packages/autohotkey-tmlanguage/test/common/declaration/function.ts
+++ packages/autohotkey-tmlanguage/test/common/declaration/function.ts
@@ -66,5 +66,68 @@ export function createFunctionDeclarationExpectedData(scopeName: ScopeName): Exp
         { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
       ],
     ],
+    [
+      dedent`
+        abc(      ; comment
+          a,      ; comment
+          b       ; comment
+          , c     ; comment
+        ) {       ; comment
+        }         ; comment
+        abc(      ; comment
+          a,      ; comment
+          b       ; comment
+          , c     ; comment
+        )         ; comment
+        {         ; comment
+        }         ; comment
+      `, [
+        { text: 'abc', scopes: name(scopeName, RuleName.FunctionName) },
+        { text: '(', scopes: name(scopeName, RuleName.OpenParen) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+        { text: 'a', scopes: name(scopeName, RuleName.Variable) },
+        { text: ',', scopes: name(scopeName, RuleName.Comma) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+        { text: 'b', scopes: name(scopeName, RuleName.Variable) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+        { text: ',', scopes: name(scopeName, RuleName.Comma) },
+        { text: 'c', scopes: name(scopeName, RuleName.Variable) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+        { text: ')', scopes: name(scopeName, RuleName.CloseParen) },
+        { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+        { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+        { text: 'abc', scopes: name(scopeName, RuleName.FunctionName) },
+        { text: '(', scopes: name(scopeName, RuleName.OpenParen) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+        { text: 'a', scopes: name(scopeName, RuleName.Variable) },
+        { text: ',', scopes: name(scopeName, RuleName.Comma) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+        { text: 'b', scopes: name(scopeName, RuleName.Variable) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+        { text: ',', scopes: name(scopeName, RuleName.Comma) },
+        { text: 'c', scopes: name(scopeName, RuleName.Variable) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+        { text: ')', scopes: name(scopeName, RuleName.CloseParen) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+        { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+        { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+      ],
+    ],
   ];
 }
