03112d45f299bfa398b7c3dbc97342afa238a2d4
zero-plusplus <git@zero-plusplus.com>
2025-05-27 22:50:51 +0900

    Refactoring

--- packages/autohotkey-tmlanguage/src/autohotkeyl/rules/legacy/assignment.ts
+++ packages/autohotkey-tmlanguage/src/autohotkeyl/rules/legacy/assignment.ts
@@ -26,7 +26,7 @@ export function createLegacyAssignmentRule(scopeName: ScopeName, placeholder: Pl
       capture(char('=')),
       inlineSpaces0(),
       capture(groupMany0(alt(
-        patterns_common.unquotedPairsStringPattern,
+        patterns_common.unquotedPairStringPattern,
         patterns_common.unquotedCharPattern,
         group(ordalt(...constants_v1.unquoteEscapeSequences)),
         char('%'),
