d6d8750f893045daaee491a4cd3299b691f32c16
zero-plusplus <git@zero-plusplus.com>
2025-05-31 22:12:49 +0900

    Update tests

--- packages/autohotkey-tmlanguage/test/common/declaration/hotkeyLabel.ts
+++ packages/autohotkey-tmlanguage/test/common/declaration/hotkeyLabel.ts
@@ -9,27 +9,35 @@ export function createHotkeyLabelStatementExpectedData(scopeName: ScopeName): Ex
   return [
     [
       dedent`
-        ~a::Return
-        $+!#abc::Return
-        LShift & Left::Return
+        a::                                   ; comment
+        ~a::Return                            ; comment
+        $+!#abc::Return                       ; comment
+        LShift & Left::Return                 ; comment
       `,
       [
+        { text: 'a', scopes: name(scopeName, RuleName.HotkeyLabelName) },
+        { text: '::', scopes: name(scopeName, RuleName.ColonColon) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
         { text: '~', scopes: name(scopeName, RuleName.HotkeyFlag) },
         { text: 'a', scopes: name(scopeName, RuleName.HotkeyLabelName) },
         { text: '::', scopes: name(scopeName, RuleName.ColonColon) },
         { text: 'Return', scopes: name(scopeName, RuleName.JumpCommandName) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
 
         { text: '$', scopes: name(scopeName, RuleName.HotkeyFlag) },
         { text: '+!#', scopes: name(scopeName, RuleName.HotkeyModifier) },
         { text: 'abc', scopes: name(scopeName, RuleName.HotkeyLabelName) },
         { text: '::', scopes: name(scopeName, RuleName.ColonColon) },
         { text: 'Return', scopes: name(scopeName, RuleName.JumpCommandName) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
 
         { text: 'LShift', scopes: name(scopeName, RuleName.HotkeyLabelName) },
         { text: '&', scopes: name(scopeName, RuleName.HotkeyCombinator) },
         { text: 'Left', scopes: name(scopeName, RuleName.HotkeyLabelName) },
         { text: '::', scopes: name(scopeName, RuleName.ColonColon) },
         { text: 'Return', scopes: name(scopeName, RuleName.JumpCommandName) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
       ],
     ],
   ];
