ce8fcb393332790019470c37cb3f2b021e76003b
zero-plusplus <git@zero-plusplus.com>
2025-05-31 21:58:26 +0900

    Update tests

--- packages/autohotkey-tmlanguage/test/common/declaration/class/method.ts
+++ packages/autohotkey-tmlanguage/test/common/declaration/class/method.ts
@@ -10,20 +10,27 @@ export function createMethodDeclarationExpectedData(scopeName: ScopeName): Expec
     // Highlight as a function name even if it has the same name as a built-in class
     [
       dedent`
-        class {
-          map() {
-          }
-        }
+        class {           ; comment
+          map() {         ; comment
+          }               ; comment
+        }                 ; comment
       `,
       [
         { text: 'class', scopes: name(scopeName, RuleName.ClassKeyword) },
         { text: '{', scopes: name(scopeName, RuleName.ClassBlockBegin) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
         { text: 'map', scopes: name(scopeName, RuleName.FunctionName) },
         { text: '(', scopes: name(scopeName, RuleName.OpenParen) },
         { text: ')', scopes: name(scopeName, RuleName.CloseParen) },
         { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
         { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
         { text: '}', scopes: name(scopeName, RuleName.ClassBlockEnd) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
       ],
     ],
   ];
