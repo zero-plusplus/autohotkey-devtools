adc27e0b824f9a54ec5ed41f310a201a528f6159
zero-plusplus <git@zero-plusplus.com>
2025-06-01 21:08:12 +0900

    Update tests

--- packages/autohotkey-tmlanguage/test/common/expression/array.ts
+++ packages/autohotkey-tmlanguage/test/common/expression/array.ts
@@ -7,101 +7,166 @@ import type { ExpectedTestData } from '../../types';
 
 export function createArrayLiteralExpectedData(scopeName: ScopeName): ExpectedTestData[] {
   return [
-    [
-      dedent`
-        abc := []
-        abc := [
-        ]
-      `, [
-        ...repeatArray(2, [
-          { text: 'abc', scopes: name(scopeName, RuleName.Variable) },
-          { text: ':=', scopes: name(scopeName, RuleName.Operator) },
-          { text: '[', scopes: name(scopeName, RuleName.OpenBracket) },
-          { text: ']', scopes: name(scopeName, RuleName.CloseBracket) },
-        ]),
-      ],
-    ],
-    [
-      dedent`
-        abc := [1]
-        abc := [ 1 ]
+    // one line
+    ...((): ExpectedTestData[] => {
+      return [
+        [
+          dedent`
+            abc := []     ; comment
+          `,
+          [
+            { text: 'abc', scopes: name(scopeName, RuleName.Variable) },
+            { text: ':=', scopes: name(scopeName, RuleName.Operator) },
+            { text: '[', scopes: name(scopeName, RuleName.OpenBracket) },
+            { text: ']', scopes: name(scopeName, RuleName.CloseBracket) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+          ],
+        ],
+        [
+          dedent`
+            abc := [1]      ; comment
+            abc := [ 1 ]    ; comment
+          `,
+          [
+            ...repeatArray(2, [
+              { text: 'abc', scopes: name(scopeName, RuleName.Variable) },
+              { text: ':=', scopes: name(scopeName, RuleName.Operator) },
+              { text: '[', scopes: name(scopeName, RuleName.OpenBracket) },
+              { text: '1', scopes: name(scopeName, RuleName.Integer) },
+              { text: ']', scopes: name(scopeName, RuleName.CloseBracket) },
+              { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+            ]),
+          ],
+        ],
+        [
+          dedent`
+            abc := [1,2]          ; comment
+            abc := [ 1, 2 ]       ; comment
+          `,
+          [
+            ...repeatArray(2, [
+              { text: 'abc', scopes: name(scopeName, RuleName.Variable) },
+              { text: ':=', scopes: name(scopeName, RuleName.Operator) },
+              { text: '[', scopes: name(scopeName, RuleName.OpenBracket) },
+              { text: '1', scopes: name(scopeName, RuleName.Integer) },
+              { text: ',', scopes: name(scopeName, RuleName.Comma) },
+              { text: '2', scopes: name(scopeName, RuleName.Integer) },
+              { text: ']', scopes: name(scopeName, RuleName.CloseBracket) },
+              { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+            ]),
+          ],
+        ],
+        [
+          dedent`
+            abc := [ ,, 1, 2,, ]        ; comment
+          `,
+          [
+            { text: 'abc', scopes: name(scopeName, RuleName.Variable) },
+            { text: ':=', scopes: name(scopeName, RuleName.Operator) },
+            { text: '[', scopes: name(scopeName, RuleName.OpenBracket) },
+            { text: ',', scopes: name(scopeName, RuleName.Comma) },
+            { text: ',', scopes: name(scopeName, RuleName.Comma) },
+            { text: '1', scopes: name(scopeName, RuleName.Integer) },
+            { text: ',', scopes: name(scopeName, RuleName.Comma) },
+            { text: '2', scopes: name(scopeName, RuleName.Integer) },
+            { text: ',', scopes: name(scopeName, RuleName.Comma) },
+            { text: ',', scopes: name(scopeName, RuleName.Comma) },
+            { text: ']', scopes: name(scopeName, RuleName.CloseBracket) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+          ],
+        ],
+        [
+          dedent`
+            abc := [ 1, [ 2 ], 3 ]      ; comment
+          `,
+          [
+            { text: 'abc', scopes: name(scopeName, RuleName.Variable) },
+            { text: ':=', scopes: name(scopeName, RuleName.Operator) },
+            { text: '[', scopes: name(scopeName, RuleName.OpenBracket) },
+            { text: '1', scopes: name(scopeName, RuleName.Integer) },
+            { text: ',', scopes: name(scopeName, RuleName.Comma) },
+            { text: '[', scopes: name(scopeName, RuleName.OpenBracket) },
+            { text: '2', scopes: name(scopeName, RuleName.Integer) },
+            { text: ']', scopes: name(scopeName, RuleName.CloseBracket) },
+            { text: ',', scopes: name(scopeName, RuleName.Comma) },
+            { text: '3', scopes: name(scopeName, RuleName.Integer) },
+            { text: ']', scopes: name(scopeName, RuleName.CloseBracket) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+          ],
+        ],
+      ];
+    })(),
 
-        abc := [1,2]
-        abc := [ 1 , 2 ]
-      `, [
-        ...repeatArray(2, [
-          { text: 'abc', scopes: name(scopeName, RuleName.Variable) },
-          { text: ':=', scopes: name(scopeName, RuleName.Operator) },
-          { text: '[', scopes: name(scopeName, RuleName.OpenBracket) },
-          { text: '1', scopes: name(scopeName, RuleName.Integer) },
-          { text: ']', scopes: name(scopeName, RuleName.CloseBracket) },
-        ]),
+    // continuation
+    ...((): ExpectedTestData[] => {
+      return [
+        [
+          dedent`
+            abc := [      ; comment
+            ]             ; comment
+          `,
+          [
 
-        ...repeatArray(2, [
-          { text: 'abc', scopes: name(scopeName, RuleName.Variable) },
-          { text: ':=', scopes: name(scopeName, RuleName.Operator) },
-          { text: '[', scopes: name(scopeName, RuleName.OpenBracket) },
-          { text: '1', scopes: name(scopeName, RuleName.Integer) },
-          { text: ',', scopes: name(scopeName, RuleName.Comma) },
-          { text: '2', scopes: name(scopeName, RuleName.Integer) },
-          { text: ']', scopes: name(scopeName, RuleName.CloseBracket) },
-        ]),
-      ],
-    ],
-    [
-      dedent`
-        abc := [ 1, [ 2 ], 3 ]
-        abc := [ 1
-               , [ 2 ]
-               , 3     ]
-      `, [
-        ...repeatArray(2, [
-          { text: 'abc', scopes: name(scopeName, RuleName.Variable) },
-          { text: ':=', scopes: name(scopeName, RuleName.Operator) },
-          { text: '[', scopes: name(scopeName, RuleName.OpenBracket) },
-          { text: '1', scopes: name(scopeName, RuleName.Integer) },
-          { text: ',', scopes: name(scopeName, RuleName.Comma) },
-          { text: '[', scopes: name(scopeName, RuleName.OpenBracket) },
-          { text: '2', scopes: name(scopeName, RuleName.Integer) },
-          { text: ']', scopes: name(scopeName, RuleName.CloseBracket) },
-          { text: ',', scopes: name(scopeName, RuleName.Comma) },
-          { text: '3', scopes: name(scopeName, RuleName.Integer) },
-          { text: ']', scopes: name(scopeName, RuleName.CloseBracket) },
-        ]),
-      ],
-    ],
+            { text: 'abc', scopes: name(scopeName, RuleName.Variable) },
+            { text: ':=', scopes: name(scopeName, RuleName.Operator) },
+            { text: '[', scopes: name(scopeName, RuleName.OpenBracket) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
 
-    // comment
-    [
-      dedent`
-        abc := [] ; inline comment
-        abc := [ ; inline comment
-          ; line comment
-          1, ; inline comment
-          /* block comment
-           */
-        ] ; inline comment
-      `, [
-        { text: 'abc', scopes: name(scopeName, RuleName.Variable) },
-        { text: ':=', scopes: name(scopeName, RuleName.Operator) },
-        { text: '[', scopes: name(scopeName, RuleName.OpenBracket) },
-        { text: ']', scopes: name(scopeName, RuleName.CloseBracket) },
-        { text: '; inline comment', scopes: name(scopeName, RuleName.InLineComment) },
+            { text: ']', scopes: name(scopeName, RuleName.CloseBracket) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+          ],
+        ],
+        [
+          dedent`
+            abc := [ 1            ; comment
+                   , [ 2 ]        ; comment
+                   , 3     ]      ; comment
+          `,
+          [
+            { text: 'abc', scopes: name(scopeName, RuleName.Variable) },
+            { text: ':=', scopes: name(scopeName, RuleName.Operator) },
+            { text: '[', scopes: name(scopeName, RuleName.OpenBracket) },
+            { text: '1', scopes: name(scopeName, RuleName.Integer) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
 
-        { text: 'abc', scopes: name(scopeName, RuleName.Variable) },
-        { text: ':=', scopes: name(scopeName, RuleName.Operator) },
-        { text: '[', scopes: name(scopeName, RuleName.OpenBracket) },
-        { text: '; inline comment', scopes: name(scopeName, RuleName.InLineComment) },
-        { text: '; line comment', scopes: name(scopeName, RuleName.SingleLineComment) },
-        { text: '1', scopes: name(scopeName, RuleName.Integer) },
-        { text: ',', scopes: name(scopeName, RuleName.Comma) },
-        { text: '; inline comment', scopes: name(scopeName, RuleName.InLineComment) },
-        { text: '/*', scopes: name(scopeName, RuleName.MultiLineComment, RuleDescriptor.Begin) },
-        { text: ' block comment', scopes: name(scopeName, RuleName.MultiLineComment) },
-        { text: '*/', scopes: name(scopeName, RuleName.MultiLineComment, RuleDescriptor.End) },
-        { text: ']', scopes: name(scopeName, RuleName.CloseBracket) },
-        { text: '; inline comment', scopes: name(scopeName, RuleName.InLineComment) },
-      ],
-    ],
+            { text: ',', scopes: name(scopeName, RuleName.Comma) },
+            { text: '[', scopes: name(scopeName, RuleName.OpenBracket) },
+            { text: '2', scopes: name(scopeName, RuleName.Integer) },
+            { text: ']', scopes: name(scopeName, RuleName.CloseBracket) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+            { text: ',', scopes: name(scopeName, RuleName.Comma) },
+            { text: '3', scopes: name(scopeName, RuleName.Integer) },
+            { text: ']', scopes: name(scopeName, RuleName.CloseBracket) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+          ],
+        ],
+        [
+          dedent`
+            abc := [ ; inline comment
+              ; line comment
+              1, ; inline comment
+              /* block comment
+               */
+            ] ; inline comment
+          `,
+          [
+            { text: 'abc', scopes: name(scopeName, RuleName.Variable) },
+            { text: ':=', scopes: name(scopeName, RuleName.Operator) },
+            { text: '[', scopes: name(scopeName, RuleName.OpenBracket) },
+            { text: '; inline comment', scopes: name(scopeName, RuleName.InLineComment) },
+            { text: '; line comment', scopes: name(scopeName, RuleName.SingleLineComment) },
+            { text: '1', scopes: name(scopeName, RuleName.Integer) },
+            { text: ',', scopes: name(scopeName, RuleName.Comma) },
+            { text: '; inline comment', scopes: name(scopeName, RuleName.InLineComment) },
+            { text: '/*', scopes: name(scopeName, RuleName.MultiLineComment, RuleDescriptor.Begin) },
+            { text: ' block comment', scopes: name(scopeName, RuleName.MultiLineComment) },
+            { text: '*/', scopes: name(scopeName, RuleName.MultiLineComment, RuleDescriptor.End) },
+            { text: ']', scopes: name(scopeName, RuleName.CloseBracket) },
+            { text: '; inline comment', scopes: name(scopeName, RuleName.InLineComment) },
+          ],
+        ],
+      ];
+    })(),
   ];
 }
