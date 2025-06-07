ebc65ed4d1d58bf5e64698e814893bb573061fcb
zero-plusplus <git@zero-plusplus.com>
2025-06-07 18:39:20 +0900

    Update tests

--- packages/autohotkey-tmlanguage/test/common/statement/jump.ts
+++ packages/autohotkey-tmlanguage/test/common/statement/jump.ts
@@ -11,27 +11,50 @@ export function createJumpStatementExpectedData(scopeName: ScopeName): ExpectedT
       return [
         [
           dedent`
-            ${command}
-            ${command} label
-            ${command}, label
-
-            {
-              ${command}
-            }
+            ${command}                        ; comment
+            ${command} label                  ; comment
+            ${command}, label                 ; comment
           `,
           [
             { text: command, scopes: name(scopeName, RuleName.JumpCommandName) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
 
             { text: command, scopes: name(scopeName, RuleName.JumpCommandName) },
             { text: 'label', scopes: name(scopeName, RuleName.LabelName) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
 
             { text: command, scopes: name(scopeName, RuleName.JumpCommandName) },
             { text: ',', scopes: name(scopeName, RuleName.Comma) },
             { text: 'label', scopes: name(scopeName, RuleName.LabelName) },
-
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+          ],
+        ],
+        [
+          dedent`
+            {                                   ; comment
+              ${command}                        ; comment
+              ${command} label                  ; comment
+              ${command}, label                 ; comment
+            }                                   ; comment
+          `,
+          [
             { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
             { text: command, scopes: name(scopeName, RuleName.JumpCommandName) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+            { text: command, scopes: name(scopeName, RuleName.JumpCommandName) },
+            { text: 'label', scopes: name(scopeName, RuleName.LabelName) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+            { text: command, scopes: name(scopeName, RuleName.JumpCommandName) },
+            { text: ',', scopes: name(scopeName, RuleName.Comma) },
+            { text: 'label', scopes: name(scopeName, RuleName.LabelName) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
             { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
           ],
         ],
       ];
@@ -40,51 +63,62 @@ export function createJumpStatementExpectedData(scopeName: ScopeName): ExpectedT
       return [
         [
           dedent`
-            ${command}
-            ${command} 1
-            ${command}, 1
-            ${command} (
-            )
-            ${command}, (
-            )
-
-            ${command} {
-              key: value
-            }
-
-            {
-              ${command}
-            }
+            ${command}                ; comment
+            ${command} 1              ; comment
+            ${command}, 1             ; comment
           `,
           [
             { text: command, scopes: name(scopeName, RuleName.JumpCommandName) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
 
             { text: command, scopes: name(scopeName, RuleName.JumpCommandName) },
             { text: '1', scopes: name(scopeName, RuleName.Integer) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
 
             { text: command, scopes: name(scopeName, RuleName.JumpCommandName) },
             { text: ',', scopes: name(scopeName, RuleName.Comma) },
             { text: '1', scopes: name(scopeName, RuleName.Integer) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+          ],
+        ],
+        [
+          dedent`
+            ${command} (          ; comment
+            )                     ; comment
+            ${command}, (         ; comment
+            )                     ; comment
 
+            ${command} {          ; comment
+              key: value          ; comment
+            }                     ; comment
+          `,
+          [
             { text: command, scopes: name(scopeName, RuleName.JumpCommandName) },
             { text: '(', scopes: name(scopeName, RuleName.OpenParen) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
             { text: ')', scopes: name(scopeName, RuleName.CloseParen) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
 
             { text: command, scopes: name(scopeName, RuleName.JumpCommandName) },
             { text: ',', scopes: name(scopeName, RuleName.Comma) },
             { text: '(', scopes: name(scopeName, RuleName.OpenParen) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
             { text: ')', scopes: name(scopeName, RuleName.CloseParen) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
 
             { text: command, scopes: name(scopeName, RuleName.JumpCommandName) },
             { text: '{', scopes: name(scopeName, RuleName.OpenBrace) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
             { text: 'key', scopes: name(scopeName, RuleName.Variable) },
             { text: ':', scopes: name(scopeName, RuleName.Colon) },
             { text: 'value', scopes: name(scopeName, RuleName.Variable) },
-            { text: '}', scopes: name(scopeName, RuleName.CloseBrace) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
 
-            { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
-            { text: command, scopes: name(scopeName, RuleName.JumpCommandName) },
-            { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+            { text: '}', scopes: name(scopeName, RuleName.CloseBrace) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
           ],
         ],
       ];
