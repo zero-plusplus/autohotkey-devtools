2fcf2620d5bb43e0c875021bed5080f806518400
zero-plusplus <git@zero-plusplus.com>
2025-06-07 18:26:47 +0900

    Update tests

--- packages/autohotkey-tmlanguage/test/common/statement/for.ts
+++ packages/autohotkey-tmlanguage/test/common/statement/for.ts
@@ -7,63 +7,124 @@ import type { ExpectedTestData } from '../../types';
 
 export function createForStatementExpectedData(scopeName: ScopeName): ExpectedTestData[] {
   return [
-    [
-      dedent`
-        for key, value in obj {
-        }
-        for key, value in obj
-        {
-        }
-      `,
-      [
-        { text: 'for', scopes: name(scopeName, RuleName.ControlFlowKeyword) },
-        { text: 'key', scopes: name(scopeName, RuleName.Variable) },
-        { text: ',', scopes: name(scopeName, RuleName.Comma) },
-        { text: 'value', scopes: name(scopeName, RuleName.Variable) },
-        { text: 'in', scopes: name(scopeName, RuleName.KeywordInExpression) },
-        { text: 'obj', scopes: name(scopeName, RuleName.Variable) },
-        { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
-        { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+    // one line
+    ...((): ExpectedTestData[] => {
+      return [
+        [
+          dedent`
+            for key, value in obj {     ; comment
+            }                           ; comment
+          `,
+          [
+            { text: 'for', scopes: name(scopeName, RuleName.ControlFlowKeyword) },
+            { text: 'key', scopes: name(scopeName, RuleName.Variable) },
+            { text: ',', scopes: name(scopeName, RuleName.Comma) },
+            { text: 'value', scopes: name(scopeName, RuleName.Variable) },
+            { text: 'in', scopes: name(scopeName, RuleName.KeywordInExpression) },
+            { text: 'obj', scopes: name(scopeName, RuleName.Variable) },
+            { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
 
-        { text: 'for', scopes: name(scopeName, RuleName.ControlFlowKeyword) },
-        { text: 'key', scopes: name(scopeName, RuleName.Variable) },
-        { text: ',', scopes: name(scopeName, RuleName.Comma) },
-        { text: 'value', scopes: name(scopeName, RuleName.Variable) },
-        { text: 'in', scopes: name(scopeName, RuleName.KeywordInExpression) },
-        { text: 'obj', scopes: name(scopeName, RuleName.Variable) },
-        { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
-        { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
-      ],
-    ],
-    [
-      dedent`
-        for, key, value in obj {
-        }
-        for, key, value in obj
-        {
-        }
-      `,
-      [
-        { text: 'for', scopes: name(scopeName, RuleName.ControlFlowKeyword) },
-        { text: ',', scopes: name(scopeName, RuleName.Comma) },
-        { text: 'key', scopes: name(scopeName, RuleName.Variable) },
-        { text: ',', scopes: name(scopeName, RuleName.Comma) },
-        { text: 'value', scopes: name(scopeName, RuleName.Variable) },
-        { text: 'in', scopes: name(scopeName, RuleName.KeywordInExpression) },
-        { text: 'obj', scopes: name(scopeName, RuleName.Variable) },
-        { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
-        { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+            { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+          ],
+        ],
+        [
+          dedent`
+            for, key, value in obj {        ; comment
+            }                               ; comment
+          `,
+          [
+            { text: 'for', scopes: name(scopeName, RuleName.ControlFlowKeyword) },
+            { text: ',', scopes: name(scopeName, RuleName.Comma) },
+            { text: 'key', scopes: name(scopeName, RuleName.Variable) },
+            { text: ',', scopes: name(scopeName, RuleName.Comma) },
+            { text: 'value', scopes: name(scopeName, RuleName.Variable) },
+            { text: 'in', scopes: name(scopeName, RuleName.KeywordInExpression) },
+            { text: 'obj', scopes: name(scopeName, RuleName.Variable) },
+            { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
 
-        { text: 'for', scopes: name(scopeName, RuleName.ControlFlowKeyword) },
-        { text: ',', scopes: name(scopeName, RuleName.Comma) },
-        { text: 'key', scopes: name(scopeName, RuleName.Variable) },
-        { text: ',', scopes: name(scopeName, RuleName.Comma) },
-        { text: 'value', scopes: name(scopeName, RuleName.Variable) },
-        { text: 'in', scopes: name(scopeName, RuleName.KeywordInExpression) },
-        { text: 'obj', scopes: name(scopeName, RuleName.Variable) },
-        { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
-        { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
-      ],
-    ],
+            { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+          ],
+        ],
+        [
+          dedent`
+            for, key, value, extra1, extra2 in obj {        ; comment
+            }                                               ; comment
+          `,
+          [
+            { text: 'for', scopes: name(scopeName, RuleName.ControlFlowKeyword) },
+            { text: ',', scopes: name(scopeName, RuleName.Comma) },
+            { text: 'key', scopes: name(scopeName, RuleName.Variable) },
+            { text: ',', scopes: name(scopeName, RuleName.Comma) },
+            { text: 'value', scopes: name(scopeName, RuleName.Variable) },
+            { text: ',', scopes: name(scopeName, RuleName.Comma) },
+            { text: 'extra1', scopes: name(scopeName, RuleName.Variable) },
+            { text: ',', scopes: name(scopeName, RuleName.Comma) },
+            { text: 'extra2', scopes: name(scopeName, RuleName.Variable) },
+            { text: 'in', scopes: name(scopeName, RuleName.KeywordInExpression) },
+            { text: 'obj', scopes: name(scopeName, RuleName.Variable) },
+            { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+            { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+          ],
+        ],
+      ];
+    })(),
+
+    // continuation
+    ...((): ExpectedTestData[] => {
+      return [
+        [
+          dedent`
+            for key, value in obj             ; comment
+            {                                 ; comment
+            }                                 ; comment
+          `,
+          [
+            { text: 'for', scopes: name(scopeName, RuleName.ControlFlowKeyword) },
+            { text: 'key', scopes: name(scopeName, RuleName.Variable) },
+            { text: ',', scopes: name(scopeName, RuleName.Comma) },
+            { text: 'value', scopes: name(scopeName, RuleName.Variable) },
+            { text: 'in', scopes: name(scopeName, RuleName.KeywordInExpression) },
+            { text: 'obj', scopes: name(scopeName, RuleName.Variable) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+            { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+            { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+          ],
+        ],
+        [
+          dedent`
+            for, key, value in obj        ; comment
+            {                             ; comment
+            }                             ; comment
+          `,
+          [
+            { text: 'for', scopes: name(scopeName, RuleName.ControlFlowKeyword) },
+            { text: ',', scopes: name(scopeName, RuleName.Comma) },
+            { text: 'key', scopes: name(scopeName, RuleName.Variable) },
+            { text: ',', scopes: name(scopeName, RuleName.Comma) },
+            { text: 'value', scopes: name(scopeName, RuleName.Variable) },
+            { text: 'in', scopes: name(scopeName, RuleName.KeywordInExpression) },
+            { text: 'obj', scopes: name(scopeName, RuleName.Variable) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+            { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+            { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+            { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+          ],
+        ],
+      ];
+    })(),
   ];
 }
