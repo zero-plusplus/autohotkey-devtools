9b0922bf78db92d0b2cd7cfe744db6a76571b6f3
zero-plusplus <git@zero-plusplus.com>
2025-06-08 01:15:29 +0900

    Update tests

--- packages/autohotkey-tmlanguage/test/autohotkeyl/expected/legacy/if.ts
+++ packages/autohotkey-tmlanguage/test/autohotkeyl/expected/legacy/if.ts
@@ -170,127 +170,23 @@ export function createLegacyIfStatementExpectedData(scopeName: ScopeName): Expec
       ];
     })(),
 
-    // #region [IfIs](https://www.autohotkey.com/docs/v1/lib/IfIs.htm)
-    [
-      dedent`
-        if value is                     ; comment
-        {                               ; comment
-        }                               ; comment
-        if value is not                 ; comment
-        {                               ; comment
-        }                               ; comment
-
-        if value is integer             ; comment
-        {                               ; comment
-        }                               ; comment
-        if value is not integer         ; comment
-        {                               ; comment
-        }                               ; comment
-
-        if value is float               ; comment
-        {                               ; comment
-        }                               ; comment
-        if value is not float           ; comment
-        {                               ; comment
-        }                               ; comment
-
-        if value is number              ; comment
-        {                               ; comment
-        }                               ; comment
-        if value is not number          ; comment
-        {                               ; comment
-        }                               ; comment
-
-        if value is digit               ; comment
-        {                               ; comment
-        }                               ; comment
-        if value is not digit           ; comment
-        {                               ; comment
-        }                               ; comment
-
-        if value is xdigit              ; comment
-        {                               ; comment
-        }                               ; comment
-        if value is not xdigit          ; comment
-        {                               ; comment
-        }                               ; comment
-
-        if value is alpha               ; comment
-        {                               ; comment
-        }                               ; comment
-        if value is not alpha           ; comment
-        {                               ; comment
-        }                               ; comment
-
-        if value is upper               ; comment
-        {                               ; comment
-        }                               ; comment
-        if value is not upper           ; comment
-        {                               ; comment
-        }                               ; comment
-
-        if value is lower               ; comment
-        {                               ; comment
-        }                               ; comment
-        if value is not lower           ; comment
-        {                               ; comment
-        }                               ; comment
-
-        if value is alnum               ; comment
-        {                               ; comment
-        }                               ; comment
-        if value is not alnum           ; comment
-        {                               ; comment
-        }                               ; comment
-
-        if value is space               ; comment
-        {                               ; comment
-        }                               ; comment
-        if value is not space           ; comment
-        {                               ; comment
-        }                               ; comment
-
-        if value is time                ; comment
-        {                               ; comment
-        }                               ; comment
-        if value is not time            ; comment
-        {                               ; comment
-        }                               ; comment
-      `, [
-        ...[
-          { text: 'if', scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.ControlFlowKeyword) },
-          { text: 'value', scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.Variable) },
-          { text: 'is', scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.KeywordInExpression) },
-          { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
-
-          { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
-          { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
-
-          { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
-          { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
-        ],
-
-        ...[
-          { text: 'if', scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.ControlFlowKeyword) },
-          { text: 'value', scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.Variable) },
-          { text: 'is', scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.KeywordInExpression) },
-          { text: 'not', scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.KeywordInExpression) },
-          { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
-
-          { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
-          { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
-
-          { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
-          { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
-        ],
-
-        ...[ 'integer', 'float', 'number', 'digit', 'xdigit', 'alpha', 'upper', 'lower', 'alnum', 'space', 'time' ].flatMap((word) => {
-          return [
+    // [IfIs](https://www.autohotkey.com/docs/v1/lib/IfIs.htm)
+    ...((): ExpectedTestData[] => {
+      return [
+        [
+          dedent`
+            if value is                     ; comment
+            {                               ; comment
+            }                               ; comment
+            if value is not                 ; comment
+            {                               ; comment
+            }                               ; comment
+          `,
+          [
             ...[
               { text: 'if', scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.ControlFlowKeyword) },
               { text: 'value', scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.Variable) },
               { text: 'is', scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.KeywordInExpression) },
-              { text: word, scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.KeywordLikeBuiltInVariable) },
               { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
 
               { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
@@ -299,12 +195,12 @@ export function createLegacyIfStatementExpectedData(scopeName: ScopeName): Expec
               { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
               { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
             ],
+
             ...[
               { text: 'if', scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.ControlFlowKeyword) },
               { text: 'value', scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.Variable) },
               { text: 'is', scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.KeywordInExpression) },
               { text: 'not', scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.KeywordInExpression) },
-              { text: word, scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.KeywordLikeBuiltInVariable) },
               { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
 
               { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
@@ -313,11 +209,143 @@ export function createLegacyIfStatementExpectedData(scopeName: ScopeName): Expec
               { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
               { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
             ],
-          ];
-        }),
-      ],
-    ],
-    // #endregion IfIs
+          ],
+        ],
+        [
+          dedent`
+            if value is integer             ; comment
+            {                               ; comment
+            }                               ; comment
+
+            if value is float               ; comment
+            {                               ; comment
+            }                               ; comment
+
+            if value is number              ; comment
+            {                               ; comment
+            }                               ; comment
+
+            if value is digit               ; comment
+            {                               ; comment
+            }                               ; comment
+
+            if value is xdigit              ; comment
+            {                               ; comment
+            }                               ; comment
+
+            if value is alpha               ; comment
+            {                               ; comment
+            }                               ; comment
+
+            if value is upper               ; comment
+            {                               ; comment
+            }                               ; comment
+
+            if value is lower               ; comment
+            {                               ; comment
+            }                               ; comment
+
+            if value is alnum               ; comment
+            {                               ; comment
+            }                               ; comment
+
+            if value is space               ; comment
+            {                               ; comment
+            }                               ; comment
+
+            if value is time                ; comment
+            {                               ; comment
+            }                               ; comment
+          `,
+          [
+            ...[ 'integer', 'float', 'number', 'digit', 'xdigit', 'alpha', 'upper', 'lower', 'alnum', 'space', 'time' ].flatMap((word) => {
+              return [
+                ...[
+                  { text: 'if', scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.ControlFlowKeyword) },
+                  { text: 'value', scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.Variable) },
+                  { text: 'is', scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.KeywordInExpression) },
+                  { text: word, scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.KeywordLikeBuiltInVariable) },
+                  { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+                  { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
+                  { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+                  { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+                  { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+                ],
+              ];
+            }),
+          ],
+        ],
+        [
+          dedent`
+            if value is not integer         ; comment
+            {                               ; comment
+            }                               ; comment
+
+            if value is not float           ; comment
+            {                               ; comment
+            }                               ; comment
+
+            if value is not number          ; comment
+            {                               ; comment
+            }                               ; comment
+
+            if value is not digit           ; comment
+            {                               ; comment
+            }                               ; comment
+
+            if value is not xdigit          ; comment
+            {                               ; comment
+            }                               ; comment
+
+            if value is not alpha           ; comment
+            {                               ; comment
+            }                               ; comment
+
+            if value is not upper           ; comment
+            {                               ; comment
+            }                               ; comment
+
+            if value is not lower           ; comment
+            {                               ; comment
+            }                               ; comment
+
+            if value is not alnum           ; comment
+            {                               ; comment
+            }                               ; comment
+
+            if value is not space           ; comment
+            {                               ; comment
+            }                               ; comment
+
+            if value is not time            ; comment
+            {                               ; comment
+            }                               ; comment
+          `,
+          [
+            ...[ 'integer', 'float', 'number', 'digit', 'xdigit', 'alpha', 'upper', 'lower', 'alnum', 'space', 'time' ].flatMap((word) => {
+              return [
+                ...[
+                  { text: 'if', scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.ControlFlowKeyword) },
+                  { text: 'value', scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.Variable) },
+                  { text: 'is', scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.KeywordInExpression) },
+                  { text: 'not', scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.KeywordInExpression) },
+                  { text: word, scopes: name(scopeName, Repository.LegacyIfStatement, RuleName.KeywordLikeBuiltInVariable) },
+                  { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+                  { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
+                  { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+                  { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+                  { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+                ],
+              ];
+            }),
+          ],
+        ],
+      ];
+    })(),
 
     // #region [IfIn](https://www.autohotkey.com/docs/v1/lib/IfIn.htm)
     ...[ 'in', 'contains' ].map((keyword): ExpectedTestData => {
