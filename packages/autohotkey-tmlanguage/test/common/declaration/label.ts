9d771b8aac35e71010915c065069978327aba7e1
zero-plusplus <git@zero-plusplus.com>
2025-06-08 02:12:41 +0900

    Update tests

--- packages/autohotkey-tmlanguage/test/common/declaration/label.ts
+++ packages/autohotkey-tmlanguage/test/common/declaration/label.ts
@@ -27,5 +27,66 @@ export function createLabelStatementExpectedData(scopeName: ScopeName): Expected
         { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
       ],
     ],
+    [
+      dedent`
+        abc:              ; comment
+          return          ; comment
+      `,
+      [
+        { text: 'abc', scopes: name(scopeName, RuleName.LabelName) },
+        { text: ':', scopes: name(scopeName, RuleName.Colon) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+        { text: 'return', scopes: name(scopeName, RuleName.JumpCommandName) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+      ],
+    ],
+    [
+      dedent`
+        abc:          ; comment
+        {             ; comment
+          return      ; comment
+        }             ; comment
+      `,
+      [
+        { text: 'abc', scopes: name(scopeName, RuleName.LabelName) },
+        { text: ':', scopes: name(scopeName, RuleName.Colon) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+        { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+        { text: 'return', scopes: name(scopeName, RuleName.JumpCommandName) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+        { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+      ],
+    ],
+    [
+      dedent`
+        abc:            ; comment
+          abc(){        ; comment
+            return      ; comment
+          }             ; comment
+      `,
+      [
+        { text: 'abc', scopes: name(scopeName, RuleName.LabelName) },
+        { text: ':', scopes: name(scopeName, RuleName.Colon) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+        { text: 'abc', scopes: name(scopeName, RuleName.FunctionName) },
+        { text: '(', scopes: name(scopeName, RuleName.OpenParen) },
+        { text: ')', scopes: name(scopeName, RuleName.CloseParen) },
+        { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+        { text: 'return', scopes: name(scopeName, RuleName.JumpCommandName) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+        { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+      ],
+    ],
   ];
 }
