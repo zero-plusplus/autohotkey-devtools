8654c357bb258df8c432062b1ac8f9ca86a03d5e
zero-plusplus <git@zero-plusplus.com>
2025-06-08 03:05:49 +0900

    Update tests

--- packages/autohotkey-tmlanguage/test/autohotkey2/expected/statement/try.ts
+++ packages/autohotkey-tmlanguage/test/autohotkey2/expected/statement/try.ts
@@ -12,22 +12,57 @@ export function createTryStatementExpectedData(scopeName: ScopeName): ExpectedTe
 
     [
       dedent`
-        try {
-        }
-        catch Error as err {
-        }
+        try {                     ; comment
+        }                         ; comment
+        catch Error {             ; comment
+        }                         ; comment
+        catch Error as {          ; comment
+        }                         ; comment
+        Catch Error As err {      ; comment
+        }                         ; comment
       `,
       [
-        { text: 'try', scopes: name(scopeName, RuleName.ControlFlowKeyword) },
-        { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
-        { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+        ...[
+          { text: 'try', scopes: name(scopeName, RuleName.ControlFlowKeyword) },
+          { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
+          { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
 
-        { text: 'catch', scopes: name(scopeName, RuleName.ControlFlowKeyword) },
-        { text: 'Error', scopes: name(scopeName, RuleName.ClassName) },
-        { text: 'as', scopes: name(scopeName, RuleName.KeywordInExpression) },
-        { text: 'err', scopes: name(scopeName, RuleName.Variable) },
-        { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
-        { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+          { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+          { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+        ],
+
+        ...[
+          { text: 'catch', scopes: name(scopeName, RuleName.ControlFlowKeyword) },
+          { text: 'Error', scopes: name(scopeName, RuleName.ClassName) },
+          { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
+          { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+          { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+          { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+        ],
+
+        ...[
+          { text: 'catch', scopes: name(scopeName, RuleName.ControlFlowKeyword) },
+          { text: 'Error', scopes: name(scopeName, RuleName.ClassName) },
+          { text: 'as', scopes: name(scopeName, RuleName.KeywordInExpression) },
+          { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
+          { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+          { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+          { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+        ],
+
+        ...[
+          { text: 'Catch', scopes: name(scopeName, RuleName.ControlFlowKeyword) },
+          { text: 'Error', scopes: name(scopeName, RuleName.ClassName) },
+          { text: 'As', scopes: name(scopeName, RuleName.KeywordInExpression) },
+          { text: 'err', scopes: name(scopeName, RuleName.Variable) },
+          { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
+          { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+          { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+          { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+        ],
       ],
     ],
   ];
