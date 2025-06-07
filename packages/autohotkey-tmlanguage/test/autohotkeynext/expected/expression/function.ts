f2f6e85fd9a5552a7fec3523c77be37b5ec3ab87
zero-plusplus <git@zero-plusplus.com>
2025-06-08 03:00:13 +0900

    Update tests

--- packages/autohotkey-tmlanguage/test/autohotkeynext/expected/expression/function.ts
+++ packages/autohotkey-tmlanguage/test/autohotkeynext/expected/expression/function.ts
@@ -9,29 +9,39 @@ export function createFunctionExpressionExpectedData(scopeName: ScopeName): Expe
   return [
     [
       dedent`
-        XXX(() {
-        })
+        XXX(() {          ; comment
+        })                ; comment
 
-        XXX(YYY() {
-        })
+        XXX(YYY() {       ; comment
+        })                ; comment
       `,
       [
-        { text: 'XXX', scopes: name(scopeName, RuleName.FunctionName) },
-        { text: '(', scopes: name(scopeName, RuleName.OpenParen) },
-        { text: '(', scopes: name(scopeName, RuleName.OpenParen) },
-        { text: ')', scopes: name(scopeName, RuleName.CloseParen) },
-        { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
-        { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
-        { text: ')', scopes: name(scopeName, RuleName.CloseParen) },
+        ...[
+          { text: 'XXX', scopes: name(scopeName, RuleName.FunctionName) },
+          { text: '(', scopes: name(scopeName, RuleName.OpenParen) },
+          { text: '(', scopes: name(scopeName, RuleName.OpenParen) },
+          { text: ')', scopes: name(scopeName, RuleName.CloseParen) },
+          { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
+          { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
 
-        { text: 'XXX', scopes: name(scopeName, RuleName.FunctionName) },
-        { text: '(', scopes: name(scopeName, RuleName.OpenParen) },
-        { text: 'YYY', scopes: name(scopeName, RuleName.FunctionName) },
-        { text: '(', scopes: name(scopeName, RuleName.OpenParen) },
-        { text: ')', scopes: name(scopeName, RuleName.CloseParen) },
-        { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
-        { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
-        { text: ')', scopes: name(scopeName, RuleName.CloseParen) },
+          { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+          { text: ')', scopes: name(scopeName, RuleName.CloseParen) },
+          { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+        ],
+
+        ...[
+          { text: 'XXX', scopes: name(scopeName, RuleName.FunctionName) },
+          { text: '(', scopes: name(scopeName, RuleName.OpenParen) },
+          { text: 'YYY', scopes: name(scopeName, RuleName.FunctionName) },
+          { text: '(', scopes: name(scopeName, RuleName.OpenParen) },
+          { text: ')', scopes: name(scopeName, RuleName.CloseParen) },
+          { text: '{', scopes: name(scopeName, RuleName.BlockBegin) },
+          { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
+          { text: '}', scopes: name(scopeName, RuleName.BlockEnd) },
+          { text: ')', scopes: name(scopeName, RuleName.CloseParen) },
+          { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+        ],
       ],
     ],
   ];
