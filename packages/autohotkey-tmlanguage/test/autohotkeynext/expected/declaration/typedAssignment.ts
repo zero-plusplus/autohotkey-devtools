a821ed401ebd4c2020eed95732874d69c241fb71
zero-plusplus <git@zero-plusplus.com>
2025-06-08 02:58:34 +0900

    Update tests

--- packages/autohotkey-tmlanguage/test/autohotkeynext/expected/declaration/typedAssignment.ts
+++ packages/autohotkey-tmlanguage/test/autohotkeynext/expected/declaration/typedAssignment.ts
@@ -9,46 +9,60 @@ export function createTypedAssignmentDeclarationExpectedData(scopeName: ScopeNam
   return [
     [
       dedent`
-        class Point {
-          x: i32
-          y: i32 := zzz
-        }
+        class Point {         ; comment
+          x: i32              ; comment
+          y: i32 := zzz       ; comment
+        }                     ; comment
       `,
       [
         { text: 'class', scopes: name(scopeName, RuleName.ClassKeyword) },
         { text: 'Point', scopes: name(scopeName, RuleName.ClassName) },
         { text: '{', scopes: name(scopeName, RuleName.ClassBlockBegin) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
         { text: 'x', scopes: name(scopeName, RuleName.Variable) },
         { text: ':', scopes: name(scopeName, RuleName.Colon) },
         { text: 'i32', scopes: name(scopeName, RuleName.Type) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
         { text: 'y', scopes: name(scopeName, RuleName.Variable) },
         { text: ':', scopes: name(scopeName, RuleName.Colon) },
         { text: 'i32', scopes: name(scopeName, RuleName.Type) },
         { text: ':=', scopes: name(scopeName, RuleName.Operator) },
         { text: 'zzz', scopes: name(scopeName, RuleName.Variable) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
         { text: '}', scopes: name(scopeName, RuleName.ClassBlockEnd) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
       ],
     ],
     [
       dedent`
-        class Point {
-          X: i32
-          Y: i32 := zzz
-        }
+        class Point {           ; comment
+          X: I32                ; comment
+          Y: I32 := zzz         ; comment
+        }                       ; comment
       `,
       [
         { text: 'class', scopes: name(scopeName, RuleName.ClassKeyword) },
         { text: 'Point', scopes: name(scopeName, RuleName.ClassName) },
         { text: '{', scopes: name(scopeName, RuleName.ClassBlockBegin) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
         { text: 'X', scopes: name(scopeName, RuleName.ConstantLikeVariable) },
         { text: ':', scopes: name(scopeName, RuleName.Colon) },
-        { text: 'i32', scopes: name(scopeName, RuleName.Type) },
+        { text: 'I32', scopes: name(scopeName, RuleName.Type) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
         { text: 'Y', scopes: name(scopeName, RuleName.ConstantLikeVariable) },
         { text: ':', scopes: name(scopeName, RuleName.Colon) },
-        { text: 'i32', scopes: name(scopeName, RuleName.Type) },
+        { text: 'I32', scopes: name(scopeName, RuleName.Type) },
         { text: ':=', scopes: name(scopeName, RuleName.Operator) },
         { text: 'zzz', scopes: name(scopeName, RuleName.Variable) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
+
         { text: '}', scopes: name(scopeName, RuleName.ClassBlockEnd) },
+        { text: '; comment', scopes: name(scopeName, RuleName.InLineComment) },
       ],
     ],
   ];
