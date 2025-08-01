20127b87f85c84d6bf242425c9514e10b0b105c9
zero-plusplus <git@zero-plusplus.com>
2025-08-02 02:20:44 +0900

    Update tests

--- packages/autohotkey-tmlanguage/test/autohotkeyl/expected/statement/directive/index.ts
+++ packages/autohotkey-tmlanguage/test/autohotkeyl/expected/statement/directive/index.ts
@@ -15,6 +15,7 @@ import { createCommentFlagExpectedDataList } from './#CommentFlag';
 import { createErrorStdOutExpectedDataList } from './#ErrorStdOut';
 import { createEscapeCharExpectedDataList } from './#EscapeChar';
 import { createHotkeyIntervalExpectedDataList } from './#HotkeyInterval';
+import { createHotkeyModifierTimeoutExpectedDataList } from './#HotkeyModifierTimeout';
 
 export function createDirectiveStatementExpectedData(scopeName: ScopeName): ExpectedTestData[] {
   return [
@@ -68,64 +69,7 @@ export function createDirectiveStatementExpectedData(scopeName: ScopeName): Expe
     ...createErrorStdOutExpectedDataList(scopeName),
     ...createEscapeCharExpectedDataList(scopeName),
     ...createHotkeyIntervalExpectedDataList(scopeName),
-
-    // https://www.autohotkey.com/docs/v1/lib/_HotkeyModifierTimeout.htm
-    ...((directiveName = '#HotkeyModifierTimeout'): ExpectedTestData[] => {
-      return [
-        [
-          dedent`
-            ${directiveName} 123        ; comment
-          `,
-          [
-            { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName) },
-            { text: '123', scopes: name(scopeName, RuleName.Integer) },
-            { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-          ],
-        ],
-        [
-          dedent`
-            ${directiveName} invalid    ; comment
-          `,
-          [
-            { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName) },
-            { text: 'invalid', scopes: name(scopeName, RuleName.UnquotedString, StyleName.Invalid) },
-            { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-          ],
-        ],
-        [
-          dedent`
-            ${directiveName}, % var                 ; comment
-            ${directiveName}, %var%                 ; comment
-            ${directiveName}, %var%var%var%         ; comment
-          `,
-          [
-            { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName) },
-            { text: ',', scopes: name(scopeName, RuleName.Comma) },
-            { text: '%', scopes: name(scopeName, RuleName.PercentExpressionBegin) },
-            { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-            { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-
-            { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName) },
-            { text: ',', scopes: name(scopeName, RuleName.Comma) },
-            { text: '%', scopes: name(scopeName, RuleName.PercentBegin) },
-            { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-            { text: '%', scopes: name(scopeName, RuleName.PercentEnd) },
-            { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-
-            { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName) },
-            { text: ',', scopes: name(scopeName, RuleName.Comma) },
-            { text: '%', scopes: name(scopeName, RuleName.PercentBegin) },
-            { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-            { text: '%', scopes: name(scopeName, RuleName.PercentBegin) },
-            { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-            { text: '%', scopes: name(scopeName, RuleName.PercentEnd) },
-            { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-            { text: '%', scopes: name(scopeName, RuleName.PercentEnd) },
-            { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-          ],
-        ],
-      ];
-    })(),
+    ...createHotkeyModifierTimeoutExpectedDataList(scopeName),
 
     // https://www.autohotkey.com/docs/v1/lib/_Hotstring.htm
     ...((directiveName = '#Hotstring'): ExpectedTestData[] => {
