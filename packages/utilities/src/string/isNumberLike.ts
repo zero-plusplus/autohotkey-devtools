63ea2df10588d251c580fd53201aca623cfb1578
zero-plusplus <git@zero-plusplus.com>
2025-08-02 21:14:25 +0900

    Add isNumberLike

---
+++ packages/utilities/src/string/isNumberLike.ts
@@ -0,0 +1,24 @@
+/**
+ * Checks if the specified value can be converted to a number.
+ * @param {unknown} value
+ * @return {boolean}
+ */
+export function isNumberLike(value: unknown): boolean {
+  switch (typeof value) {
+    case 'number': return isFinite(value);
+    case 'bigint': return isFinite(Number(value));
+    case 'string':
+    {
+      // Blank spaces are converted to 0, so exclude them
+      if (value.trim() === '') {
+        return false;
+      }
+
+      const converted = Number(value);
+      return !isNaN(converted) && isFinite(converted);
+    }
+    default: break;
+  }
+
+  return false;
+}
