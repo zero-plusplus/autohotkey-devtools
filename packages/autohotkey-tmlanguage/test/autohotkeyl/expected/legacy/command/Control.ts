bf5714c76e9d8d95bad194214f20c9ab7ef5a8a4
zero-plusplus <git@zero-plusplus.com>
2025-08-05 03:43:25 +0900

    Update tests

--- packages/autohotkey-tmlanguage/test/autohotkeyl/expected/legacy/command/Control.ts
+++ packages/autohotkey-tmlanguage/test/autohotkeyl/expected/legacy/command/Control.ts
@@ -563,7 +563,7 @@ export function createControlExpectedDataList(scopeName: ScopeName): ExpectedTes
       return [
         [
           dedent`
-            ${commandName} ${subcommand},,,,,, unquoted          ; comment
+            ${commandName} ${subcommand},,,,,, unquoted,          ; comment
           `,
           [
             { text: commandName, scopes: name(scopeName, RuleName.CommandName) },
@@ -574,7 +574,7 @@ export function createControlExpectedDataList(scopeName: ScopeName): ExpectedTes
             { text: ',', scopes: name(scopeName, RuleName.Comma) },
             { text: ',', scopes: name(scopeName, RuleName.Comma) },
             { text: ',', scopes: name(scopeName, RuleName.Comma) },
-            { text: 'unquoted', scopes: name(scopeName, RuleName.UnquotedString) },
+            { text: 'unquoted,', scopes: name(scopeName, RuleName.UnquotedString) },
             { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
           ],
         ],
