30efd7345dda4f0b7687306b01ca8d25450a45f0
zero-plusplus <git@zero-plusplus.com>
2025-08-28 01:53:11 +0900

    Update tests

--- packages/autohotkey-tmlanguage/test/autohotkeyl/expected/statement/directive/#ErrorStdOut.ts
+++ packages/autohotkey-tmlanguage/test/autohotkeyl/expected/statement/directive/#ErrorStdOut.ts
@@ -1,73 +1,10 @@
-import { dedent } from '@zero-plusplus/utilities/src';
-import { name, RuleName, StyleName, type ScopeName } from '../../../../../src/tmlanguage';
+import { RuleName, type ScopeName } from '../../../../../src/tmlanguage';
+import { $encoding } from '../../../../helpers/definition/parameter/$encoding';
 import type { ExpectedTestData } from '../../../../types';
 
 // https://www.autohotkey.com/docs/v1/lib/_ErrorStdOut.htm
 export function createErrorStdOutExpectedDataList(scopeName: ScopeName): ExpectedTestData[] {
   const directiveName = '#ErrorStdOut';
 
-  return [
-    [
-      dedent`
-        ${directiveName} x          ; comment
-        ${directiveName}, x         ; comment
-      `,
-      [
-        { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName) },
-        { text: 'x', scopes: name(scopeName, RuleName.UnquotedString) },
-        { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-
-        { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName) },
-        { text: ',', scopes: name(scopeName, RuleName.Comma) },
-        { text: 'x', scopes: name(scopeName, RuleName.UnquotedString) },
-        { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-      ],
-    ],
-    ...[ 'UTF-8', 'CP65001' ].flatMap((param): ExpectedTestData[] => {
-      return [
-        [
-          dedent`
-            ${directiveName} ${param}        ; comment
-          `,
-          [
-            { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName) },
-            { text: param, scopes: name(scopeName, RuleName.UnquotedString, StyleName.Strong) },
-            { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-          ],
-        ],
-      ];
-    }),
-    [
-      dedent`
-        ${directiveName}, % var                 ; comment
-        ${directiveName}, %var%                 ; comment
-        ${directiveName}, %var%var%var%         ; comment
-      `,
-      [
-        { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName) },
-        { text: ',', scopes: name(scopeName, RuleName.Comma) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentExpressionBegin) },
-        { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-        { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-
-        { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName) },
-        { text: ',', scopes: name(scopeName, RuleName.Comma) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentBegin) },
-        { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentEnd) },
-        { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-
-        { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName) },
-        { text: ',', scopes: name(scopeName, RuleName.Comma) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentBegin) },
-        { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentBegin) },
-        { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentEnd) },
-        { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentEnd) },
-        { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-      ],
-    ],
-  ];
+  return [ ...$encoding(scopeName, { name: directiveName, elementName: RuleName.DirectiveName, index: 0 }) ];
 }
