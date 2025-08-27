4d0455c1964f4fae215814e07c292bebe107d9bb
zero-plusplus <git@zero-plusplus.com>
2025-08-28 01:42:02 +0900

    Update tests

--- packages/autohotkey-tmlanguage/test/autohotkeyl/expected/statement/directive/#CommentFlag.ts
+++ packages/autohotkey-tmlanguage/test/autohotkeyl/expected/statement/directive/#CommentFlag.ts
@@ -1,53 +1,10 @@
-import { dedent } from '@zero-plusplus/utilities/src';
-import { name, RuleName, StyleName, type ScopeName } from '../../../../../src/tmlanguage';
+import { RuleName, type ScopeName } from '../../../../../src/tmlanguage';
+import { $ } from '../../../../helpers/definition/parameter/$';
 import type { ExpectedTestData } from '../../../../types';
 
 // https://www.autohotkey.com/docs/v1/lib/_CommentFlag.htm
 export function createCommentFlagExpectedDataList(scopeName: ScopeName): ExpectedTestData[] {
   const directiveName = '#CommentFlag';
 
-  return [
-    [
-      dedent`
-        ${directiveName} //       ; comment
-      `,
-      [
-        { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName, StyleName.Strikethrough) },
-        { text: '//', scopes: name(scopeName, RuleName.UnquotedString) },
-        { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-      ],
-    ],
-    [
-      dedent`
-        ${directiveName}, % var                 ; comment
-        ${directiveName}, %var%                 ; comment
-        ${directiveName}, %var%var%var%         ; comment
-      `,
-      [
-        { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName, StyleName.Strikethrough) },
-        { text: ',', scopes: name(scopeName, RuleName.Comma) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentExpressionBegin) },
-        { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-        { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-
-        { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName, StyleName.Strikethrough) },
-        { text: ',', scopes: name(scopeName, RuleName.Comma) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentBegin) },
-        { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentEnd) },
-        { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-
-        { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName, StyleName.Strikethrough) },
-        { text: ',', scopes: name(scopeName, RuleName.Comma) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentBegin) },
-        { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentBegin) },
-        { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentEnd) },
-        { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentEnd) },
-        { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-      ],
-    ],
-  ];
+  return [ ...$(scopeName, { name: directiveName, elementName: RuleName.DirectiveName, index: 0, deprecated: true }) ];
 }
