c526927834e983f66781605037964b8aabf34609
zero-plusplus <git@zero-plusplus.com>
2025-08-28 02:02:43 +0900

    Update tests

--- packages/autohotkey-tmlanguage/test/autohotkeyl/expected/statement/directive/#MaxMem.ts
+++ packages/autohotkey-tmlanguage/test/autohotkeyl/expected/statement/directive/#MaxMem.ts
@@ -1,61 +1,10 @@
-import { dedent } from '@zero-plusplus/utilities/src';
-import { name, RuleName, type ScopeName } from '../../../../../src/tmlanguage';
+import { RuleName, type ScopeName } from '../../../../../src/tmlanguage';
+import { $shouldInteger } from '../../../../helpers/definition/parameter/$shouldInteger';
 import type { ExpectedTestData } from '../../../../types';
 
 // https://www.autohotkey.com/docs/v1/lib/_MaxMem.htm
 export function createMaxMemExpectedDataList(scopeName: ScopeName): ExpectedTestData[] {
   const directiveName = '#MaxMem';
 
-  return [
-    ...[ '0', '50', '500', '4095' ].map((param): ExpectedTestData => {
-      return [
-        dedent`
-          ${directiveName} ${param}        ; comment
-          ${directiveName}, ${param}       ; comment
-        `,
-        [
-          { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName) },
-          { text: param, scopes: name(scopeName, RuleName.Integer) },
-          { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-
-          { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName) },
-          { text: ',', scopes: name(scopeName, RuleName.Comma) },
-          { text: param, scopes: name(scopeName, RuleName.Integer) },
-          { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-        ],
-      ];
-    }),
-    [
-      dedent`
-        ${directiveName}, % var                 ; comment
-        ${directiveName}, %var%                 ; comment
-        ${directiveName}, %var%var%var%         ; comment
-      `,
-      [
-        { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName) },
-        { text: ',', scopes: name(scopeName, RuleName.Comma) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentExpressionBegin) },
-        { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-        { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-
-        { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName) },
-        { text: ',', scopes: name(scopeName, RuleName.Comma) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentBegin) },
-        { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentEnd) },
-        { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-
-        { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName) },
-        { text: ',', scopes: name(scopeName, RuleName.Comma) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentBegin) },
-        { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentBegin) },
-        { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentEnd) },
-        { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentEnd) },
-        { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-      ],
-    ],
-  ];
+  return [ ...$shouldInteger(scopeName, { name: directiveName, elementName: RuleName.DirectiveName, index: 0, isLastParameter: true }) ];
 }
