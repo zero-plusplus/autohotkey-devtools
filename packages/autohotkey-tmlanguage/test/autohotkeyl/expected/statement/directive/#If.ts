65544523bc5ff799f8f3d1ab7d4bb116f1754398
zero-plusplus <git@zero-plusplus.com>
2025-08-28 02:37:36 +0900

    Update tests

--- packages/autohotkey-tmlanguage/test/autohotkeyl/expected/statement/directive/#If.ts
+++ packages/autohotkey-tmlanguage/test/autohotkeyl/expected/statement/directive/#If.ts
@@ -1,57 +1,10 @@
-import { dedent } from '@zero-plusplus/utilities/src';
-import { name, RuleDescriptor, RuleName, StyleName, type ScopeName } from '../../../../../src/tmlanguage';
+import { RuleName, type ScopeName } from '../../../../../src/tmlanguage';
+import { $expression } from '../../../../helpers/definition/parameter/$expression';
 import type { ExpectedTestData } from '../../../../types';
 
 // https://www.autohotkey.com/docs/v1/lib/_If.htm
 export function createIfExpectedDataList(scopeName: ScopeName): ExpectedTestData[] {
   const directiveName = '#If';
 
-  return [
-    [
-      dedent`
-        ${directiveName} WinActive("ahk_exe xxx.exe")        ; comment
-      `,
-      [
-        { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName) },
-        { text: 'WinActive', scopes: name(scopeName, RuleName.FunctionName) },
-        { text: '(', scopes: name(scopeName, RuleName.OpenParen) },
-        { text: '"', scopes: name(scopeName, RuleName.DoubleString, RuleDescriptor.Begin) },
-        { text: `ahk_exe xxx.exe`, scopes: name(scopeName, RuleName.DoubleString) },
-        { text: '"', scopes: name(scopeName, RuleName.DoubleString, RuleDescriptor.End) },
-        { text: ')', scopes: name(scopeName, RuleName.CloseParen) },
-        { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-      ],
-    ],
-    [
-      dedent`
-        ${directiveName}, % var                 ; comment
-        ${directiveName}, %var%                 ; comment
-        ${directiveName}, %var%var%var%         ; comment
-      `,
-      [
-        { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName) },
-        { text: ',', scopes: name(scopeName, RuleName.Comma) },
-        { text: '% var', scopes: name(scopeName, RuleName.PercentExpressionBegin, StyleName.Invalid) },
-        { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-
-        { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName) },
-        { text: ',', scopes: name(scopeName, RuleName.Comma) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentBegin) },
-        { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentEnd) },
-        { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-
-        { text: directiveName, scopes: name(scopeName, RuleName.DirectiveName) },
-        { text: ',', scopes: name(scopeName, RuleName.Comma) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentBegin) },
-        { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentBegin) },
-        { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentEnd) },
-        { text: 'var', scopes: name(scopeName, RuleName.Variable) },
-        { text: '%', scopes: name(scopeName, RuleName.PercentEnd) },
-        { text: '; comment', scopes: name(scopeName, RuleName.InlineComment) },
-      ],
-    ],
-  ];
+  return [ ...$expression(scopeName, { name: directiveName, elementName: RuleName.DirectiveName, index: 0, isLastParameter: true }) ];
 }
