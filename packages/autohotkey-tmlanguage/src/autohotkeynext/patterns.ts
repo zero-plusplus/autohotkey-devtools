97dff360f8fac540a6ea05ab687934025c6b00ad
zero-plusplus <git@zero-plusplus.com>
2025-09-09 19:11:16 +0900

    Refactoring

--- packages/autohotkey-tmlanguage/src/autohotkeynext/patterns.ts
+++ packages/autohotkey-tmlanguage/src/autohotkeynext/patterns.ts
@@ -1,30 +1,19 @@
 import * as patterns_v1 from '../autohotkeyl/patterns';
-import * as patterns_common from '../common/patterns';
 import {
-  alt,
-  group,
   ignoreCase,
-  inlineSpace,
-  inlineSpaces0,
   inlineSpaces1,
-  lookbehind,
   optseq,
   seq,
 } from '../oniguruma';
 
-export const exportStartPattern: string = group(seq(
-  lookbehind(patterns_common.lineStartPattern),
-  inlineSpaces0(),
+export const classStartPattern: string = seq(
+  patterns_v1.statementStartPattern,
   optseq(
     ignoreCase('export'),
+    inlineSpaces1(),
     optseq(
-      inlineSpaces1(),
       ignoreCase('default'),
-      inlineSpace(),
+      inlineSpaces1(),
     ),
   ),
-));
-export const classStartPattern: string = group(alt(
-  exportStartPattern,
-  patterns_v1.statementStartPattern,
-));
+);
