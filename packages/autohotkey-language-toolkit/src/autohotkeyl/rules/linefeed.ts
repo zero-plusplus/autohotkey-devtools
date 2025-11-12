2ee1f172614f1d6d685984f591982c03784e82f0
zero-plusplus <git@zero-plusplus.com>
2025-11-13 00:55:42 +0900

    Change in specification for `linefeed` token

--- packages/autohotkey-language-toolkit/src/autohotkeyl/rules/linefeed.ts
+++ packages/autohotkey-language-toolkit/src/autohotkeyl/rules/linefeed.ts
@@ -1,13 +1,8 @@
 import type { ScannerRule, Token, TokenDefinition } from '../../core/scanner/types';
 
 const tokenName = 'linefeed';
-export const scanLinefeed: TokenDefinition = ({ eof, consume, commit }): Token | undefined => {
-  while (!eof()) {
-    if (consume('\n')) {
-      continue;
-    }
-    break;
-  }
+export const scanLinefeed: TokenDefinition = ({ consume, commit }): Token | undefined => {
+  consume('\n');
   return commit(tokenName);
 };
 export const linefeedTokenRule: ScannerRule = {
--- packages/autohotkey-language-toolkit/test/autohotkeyl/rules/linefeed.test.ts
+++ packages/autohotkey-language-toolkit/test/autohotkeyl/rules/linefeed.test.ts
@@ -4,11 +4,20 @@ import { Scanner } from '../../../src/core/scanner';
 describe('linefeed', () => {
   test.each([
     '\n',
-    '\n\n\n',
   ])('pass', (text) => {
     const scanner = new Scanner(text);
     const token = scanner.scan(linefeedTokenRule);
 
     expect(token!.text).toBe(text);
+  });
+
+  test.each([
+    '\n\n\n',
+  ])('pass', (text) => {
+    const scanner = new Scanner(text);
+
+    expect(scanner.scan(linefeedTokenRule)!.text).toBe('\n');
+    expect(scanner.scan(linefeedTokenRule)!.text).toBe('\n');
+    expect(scanner.scan(linefeedTokenRule)!.text).toBe('\n');
   });
 });
