9c6c89dcc53a715b5fde8ef50c8dd05580f5cb32
zero-plusplus <git@zero-plusplus.com>
2025-11-17 22:27:07 +0900

    Add percent token rule

---
+++ packages/autohotkey-language-toolkit/src/common/rules/operator/percent.ts
@@ -0,0 +1,15 @@
+import { TokenKind } from '../../../core/scanner/constants';
+import type {
+  ScannerRule,
+  Token,
+  TokenDefinition,
+} from '../../../core/scanner/types';
+
+export const scanPercent: TokenDefinition = ({ consume, commit }): Token | undefined => {
+  consume('%');
+  return commit(TokenKind.Percent);
+};
+export const percentTokenRule: ScannerRule = {
+  kind: TokenKind.Percent,
+  scan: scanPercent,
+};
--- packages/autohotkey-language-toolkit/src/core/scanner/constants.ts
+++ packages/autohotkey-language-toolkit/src/core/scanner/constants.ts
@@ -8,4 +8,6 @@ export const enum TokenKind {
 
   String,
   Number,
+
+  Percent,
 }
